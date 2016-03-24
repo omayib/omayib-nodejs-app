752fbe7250bea0a60a18f85d1894f50b774abcde
omayib <omayib@gmail.com>
2016-03-24 12:28:43 +0700

    remove log

--- index.js
+++ index.js
@@ -29,4 +29,3 @@ app.use('/api', router);
 // START THE SERVER
 // =============================================================================
 app.listen(port);
-console.log('Magic happens on port ' + port);
